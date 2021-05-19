1106644d8fb3c41f2aacb7c686ccd39737b1fd9f
royantar0311 <royantar1@gmail.com>
2021-05-19 17:39:48 +0600

    added some files

---
+++ src/screens/index.ts
@@ -0,0 +1,11 @@
+import Home from './Home';
+import Quizzes from './Quizzes';
+import PageNotFound from './PageNotFound';
+import StartQuiz from './StartQuiz';
+
+export {
+    Home,
+    Quizzes,
+    PageNotFound,
+    StartQuiz
+}
