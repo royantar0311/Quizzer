0de7d0938e8880c086f5dff1bdff963504f37bb9
arBishal <arbishal.319@gmail.com>
2021-05-22 20:48:07 +0600

    quizattemp updated

--- src/screens/QuizAttempt.tsx
+++ src/screens/QuizAttempt.tsx
@@ -4,10 +4,23 @@ import './QuizAttempt.css'
 const QuizAttempt: FC = () => {
 
     const [question, setQuestion] = useState("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
+    const [count, setCount] = useState(3);
+    const [totalQues, setTotalQues] = useState(10);
 
     return (
-        
+
         <div>
+
+            <div className="time__count">
+                Time Elapsed: XX:XX
+            </div>
+
+            <div className="ques__count">
+                Question {count}/{totalQues}
+            </div>
+
+            <br/> <br/>
+
             <div className="question">
                 {question}
             </div>
